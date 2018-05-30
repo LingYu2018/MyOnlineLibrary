b6c15e74943b5339b8d9d93a1e8d7b41eea9e54e
LingYu2018 <39140492+LingYu2018@users.noreply.github.com>
2018-05-30 18:57:53 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>My Online Library</title>
+<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+<div class="header">
+<div class="wrapper">
+<h1 class="branding-title"><a href="img/branding-title.png" height="80">My Online Library</a></h1>
+<ul class="nav">
+<li class="books"><a href="catalog.php?cat=books">Books</a></li>
+<li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
+<li class="music"><a href="catalog.php?cat=music">Music</a></li>
+<li class="suggest"><a href="catalog.php?cat=suggest">Suggest</a></li>
+</ul>
+</div>
+</div>
+<div id="content">
---
+++ inc/data.php
@@ -0,0 +1,212 @@
+<?php
+$catalog = [];
+
+$catalog[101] = [
+    "title" => "The Book of Joy",
+    "img" => "img/media/53142-425x425-LG_Star_Signs.jpg",
+   "genre" => "Drama",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+$catalog[102] = [
+    "title" => "The Book of Joy",
+    "img" => "img/media/cropped-color-inhanced-front-cover-425x425.png",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+$catalog[103] = [
+    "title" => "The Book of Joy",
+    "img" => "img/media/event-featured-Elena-Eustaquio-1520442501-425x425.png",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+$catalog[104] = [
+    "title" => "The Book of Joy",
+    "img" => "img/media/GemsofNA1-425x425.jpg",
+   "genre" => "Self-help",
+   "format" => "Paperback",
+   "year" => 2016,
+   "category" => "Books",
+   "authors" => [
+       "Desmond Tutu",
+       "14th Dalai Lama",
+       "Douglas Abrams"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780399185045'
+];
+
+//movies
+$catalog[201] = [
+   "title" => "Avatar",
+   "img" => "img/media/primary-Riverdance---The-20th-Anniversary-World-Tour-1490547512-425x425.jpeg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+$catalog[202] = [
+   "title" => "Avatar",
+   "img" => "img/media/images (4).jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+$catalog[203] = [
+   "title" => "Avatar",
+   "img" => "img/media/images.jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+$catalog[204] = [
+   "title" => "Avatar",
+   "img" => "img/media/images (3).jpg",
+   "genre" => "Action,Adventure,Fantasy",
+   "format" => "DVD",
+   "year" => 2009,
+   "category" => "Movies",
+   "director" => "James Cameron",
+   "writers" => [
+       "James Cameron"
+
+   ],
+   "stars" => [
+       "Sam Worthington",
+       "Zoe Saldana",
+       "Sigourney Weaver"
+   ]
+];
+//music
+$catalog[301] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/bs223_bs20_cvr.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[302] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/download.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[303] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/images (1).jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[304] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/images-5.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+//suggest
+$catalog[401] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/facp_100x105_mp_web_by_kaminarivfx-d6pw6s5.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[402] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media//images-4.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[403] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/download-5.jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+$catalog[404] = [
+   "title" => "Beethoven: Complete Symphonies",
+   "img" => "img/media/images (4).jpg",
+   "genre" => "Clasical",
+   "format" => "CD",
+   "year" => 2012,
+   "category" => "Music",
+   "artist" => "Ludwig van Beethoven"
+];
+?>
